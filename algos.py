start decomposition, clip bugfix, use logprobs, add discr instance noise, ant data, add pkl to gitignore

algos.py
@@ -66,21 +66,21 @@ class AIRL:
 
     def train(self, n_iters, batch_timesteps=10000, max_ep_len=500, reward_fn=make_ent_env_reward_fn(None)):
         # AIRL: keep replay buffer of past 20 iterations of policies
-        obs_buffer, next_obs_buffer, action_log_probs_buffer = None, None, None
+        obs_buffer, next_obs_buffer, actions_buffer = None, None, None
         for iter_ in range(n_iters):
             print('______________')
             print('Iteration', iter_)
             obs, next_obs, actions, action_log_probs, values, value_targets, advantages = collect_and_process_rollouts(self.env_fn, self.policy, reward_fn, self.sess, batch_timesteps, max_ep_len)
 
             if obs_buffer is None:
-                obs_buffer, next_obs_buffer, action_log_probs_buffer = obs, next_obs, action_log_probs
+                obs_buffer, next_obs_buffer, actions_buffer = obs, next_obs, actions
             else:
-                obs_buffer, next_obs_buffer, action_log_probs_buffer = np.concatenate((obs_buffer, obs)), np.concatenate((next_obs_buffer, next_obs)), np.concatenate((action_log_probs_buffer, action_log_probs))
-                obs_buffer, next_obs_buffer, action_log_probs_buffer = obs_buffer[-20*batch_timesteps:], next_obs_buffer[-20*batch_timesteps:], action_log_probs_buffer[-20*batch_timesteps:]
+                obs_buffer, next_obs_buffer, actions_buffer = np.concatenate((obs_buffer, obs)), np.concatenate((next_obs_buffer, next_obs)), np.concatenate((actions_buffer, actions))
+                obs_buffer, next_obs_buffer, actions_buffer = obs_buffer[-20*batch_timesteps:], next_obs_buffer[-20*batch_timesteps:], actions_buffer[-20*batch_timesteps:]
 
             self.policy.optimizer.train(obs, next_obs, actions, action_log_probs, values, value_targets, advantages, self.sess)
             self.discriminator.train(
                 self.expert_obs, self.expert_next_obs, self.expert_actions,
-                obs_buffer, next_obs_buffer, action_log_probs_buffer,
+                obs_buffer, next_obs_buffer, actions_buffer,
                 self.policy, self.sess
             )

discriminators.py
@@ -16,6 +16,8 @@ class AIRLDiscriminator:
         discount=0.99,
         learning_rate=1e-3
     ):
+        self.last_loss = -np.log(0.5)
+        self.noise_param = 0
         with tf.variable_scope(name):
             self.obs = tf.placeholder(tf.float32, shape=[None, ob_dim], name='obs')
             self.next_obs = tf.placeholder(tf.float32, shape=[None, ob_dim], name='next_obs')
@@ -30,25 +32,23 @@ class AIRLDiscriminator:
 
             # estimate p(a | s, expert) as follows:
             self.expert_action_log_probs = self.rewards + discount*self.next_values - self.values
-            self.expert_action_probs = tf.exp(self.expert_action_log_probs)
 
             self.policy_action_log_probs = tf.placeholder(tf.float32, shape=[None, 1], name='policy_action_log_probs')
-            self.policy_action_probs = tf.exp(self.policy_action_log_probs)
-            self.expert_probs = self.expert_action_probs / (self.expert_action_probs + self.policy_action_probs) # assuming 50% of actions are from expert, and 50% from policy
+            self.expert_log_probs = self.expert_action_log_probs - tf.reduce_logsumexp(tf.stack((self.expert_action_log_probs, self.policy_action_log_probs)), axis=0)
 
             # training
             self.labels = tf.placeholder(tf.float32, shape=[None, 1], name='labels') # 1 if from expert else 0
             # self.expert_loss = -tf.log(self.expert_probs)
             # self.policy_loss = -tf.log(1-self.expert_probs)
-            self.loss = -tf.reduce_mean(tf.log((1-self.labels) + 2*(self.labels-0.5)*self.expert_probs + 1e-8))
+            self.loss = -tf.reduce_mean(self.labels*self.expert_log_probs + (1-self.labels)*tf.log(1-tf.exp(self.expert_log_probs)+1e-8))
             self.train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.loss)
 
-    def expert_prob(self, obs, next_obs, policy_action_log_probs, global_session):
-        expert_probs = global_session.run(
-            self.expert_probs,
+    def expert_log_prob(self, obs, next_obs, policy_action_log_probs, global_session):
+        expert_log_probs = global_session.run(
+            self.expert_log_probs,
             feed_dict={self.obs: obs, self.next_obs: next_obs, self.policy_action_log_probs: policy_action_log_probs}
         )
-        return expert_probs
+        return expert_log_probs
 
     def reward(self, obs, global_session):
         rewards = global_session.run(
@@ -59,10 +59,29 @@ class AIRLDiscriminator:
 
     def train(self,
             expert_obs, expert_next_obs, expert_actions,
-            policy_obs, policy_next_obs, policy_action_log_probs,
+            policy_obs, policy_next_obs, policy_actions,
             policy, global_session,
             n_iters=100, batch_size=32
         ):
+            # add instance noise to the inputs if the signal for the generator is too weak
+            if self.last_loss < -np.log(0.9):
+                print('increasing noise param to weaken discriminator')
+                self.noise_param += 0.01
+            else:
+                self.noise_param *= 0.9
+            expert_obs += np.random.normal(loc=0, scale=self.noise_param*np.std(expert_obs, axis=0), size=expert_obs.shape)
+            expert_next_obs += np.random.normal(loc=0, scale=self.noise_param*np.std(expert_next_obs, axis=0), size=expert_next_obs.shape)
+            expert_actions += np.random.normal(loc=0, scale=self.noise_param*np.std(expert_actions, axis=0), size=expert_actions.shape)
+            policy_obs += np.random.normal(loc=0, scale=self.noise_param*np.std(policy_obs, axis=0), size=policy_obs.shape)
+            policy_next_obs += np.random.normal(loc=0, scale=self.noise_param*np.std(policy_next_obs, axis=0), size=policy_next_obs.shape)
+            policy_actions += np.random.normal(loc=0, scale=self.noise_param*np.std(policy_actions, axis=0), size=policy_actions.shape)
+
+            policy_action_log_probs = global_session.run(
+                policy.action_log_probs,
+                feed_dict={policy.obs: policy_obs, policy.actions: policy_actions}
+            )
+            policy_action_log_probs = np.expand_dims(policy_action_log_probs, axis=1)
+
             expert_action_log_probs_under_policy = global_session.run(
                 policy.action_log_probs,
                 feed_dict={policy.obs: expert_obs, policy.actions: expert_actions}
@@ -82,8 +101,93 @@ class AIRLDiscriminator:
                     feed_dict={self.obs: mb_obs, self.next_obs: mb_next_obs, self.policy_action_log_probs: mb_policy_action_log_probs, self.labels: labels}
                 )
 
-            loss = global_session.run(
+            self.last_loss = global_session.run(
                 self.loss,
                 feed_dict={self.obs: mb_obs, self.next_obs: mb_next_obs, self.policy_action_log_probs: mb_policy_action_log_probs, self.labels: labels}
             )
-            print('discrim_loss:', loss)
+            print('discrim_loss:', self.last_loss)
+
+# class BasisDiscriminator:
+#     def __init__(
+#         self,
+#         name,
+#         ob_dim,
+#         n_tasks=4,
+#         n_timesteps=25,
+#         basis_size=4,
+#         out_activation=None,
+#         hidden_dims=[64, 64],
+#         hidden_activation=tf.nn.tanh,
+#         weight_init=tf.contrib.layers.xavier_initializer,
+#         bias_init=tf.zeros_initializer,
+#         discount=0.99,
+#         learning_rate=1e-3
+#     ):
+#         with tf.variable_scope(name):
+#             self.obs = tf.placeholder(tf.float32, shape=[None, ob_dim], name='obs')
+#             self.next_obs = tf.placeholder(tf.float32, shape=[None, ob_dim], name='next_obs')
+#             # reward network. shared accross tasks and timesteps
+#             self.basis_network = MLP('basis', ob_dim, 1, out_activation=out_activation, hidden_dims=hidden_dims, hidden_activation=hidden_activation, weight_init=weight_init, bias_init=bias_init, in_layer=self.obs)
+#             self.rewards = self.reward_network.layers['out']
+#             # w(task, timestep)
+#             self.weights =
+#
+#             # estimate p(a | s, expert) as follows:
+#             self.expert_action_log_probs = self.rewards + discount*self.next_values - self.values
+#             self.expert_action_probs = tf.exp(self.expert_action_log_probs)
+#
+#             self.policy_action_log_probs = tf.placeholder(tf.float32, shape=[None, 1], name='policy_action_log_probs')
+#             self.policy_action_probs = tf.exp(self.policy_action_log_probs)
+#             self.expert_probs = self.expert_action_probs / (self.expert_action_probs + self.policy_action_probs) # assuming 50% of actions are from expert, and 50% from policy
+#
+#             # training
+#             self.labels = tf.placeholder(tf.float32, shape=[None, 1], name='labels') # 1 if from expert else 0
+#             # self.expert_loss = -tf.log(self.expert_probs)
+#             # self.policy_loss = -tf.log(1-self.expert_probs)
+#             self.loss = -tf.reduce_mean(tf.log((1-self.labels) + 2*(self.labels-0.5)*self.expert_probs + 1e-8))
+#             self.train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.loss)
+#
+#     def expert_prob(self, task, obs, next_obs, policy_action_log_probs, global_session):
+#         expert_probs = global_session.run(
+#             self.expert_probs,
+#             feed_dict={self.obs: obs, self.next_obs: next_obs, self.policy_action_log_probs: policy_action_log_probs}
+#         )
+#         return expert_probs
+#
+#     def reward(self, task, obs, global_session):
+#         rewards = global_session.run(
+#             self.rewards,
+#             feed_dict={self.obs: obs}
+#         )
+#         return rewards
+#
+#     def train(self,
+#             expert_obs, expert_next_obs, expert_actions,
+#             policy_obs, policy_next_obs, policy_action_log_probs,
+#             policy, global_session,
+#             n_iters=100, batch_size=32
+#         ):
+#             expert_action_log_probs_under_policy = global_session.run(
+#                 policy.action_log_probs,
+#                 feed_dict={policy.obs: expert_obs, policy.actions: expert_actions}
+#             )
+#             expert_action_log_probs_under_policy = np.expand_dims(expert_action_log_probs_under_policy, axis=1)
+#
+#             labels = np.concatenate((np.ones((batch_size, 1)), np.zeros((batch_size, 1))))
+#
+#             for iter_ in range(n_iters):
+#                 mb_expert_obs, mb_expert_next_obs, mb_expert_action_log_probs_under_policy = sample_minibatch(expert_obs, expert_next_obs, expert_action_log_probs_under_policy, batch_size)
+#                 mb_policy_obs, mb_policy_next_obs, mb_policy_action_log_probs = sample_minibatch(policy_obs, policy_next_obs, policy_action_log_probs, batch_size)
+#                 mb_obs = np.concatenate((mb_expert_obs, mb_policy_obs))
+#                 mb_next_obs = np.concatenate((mb_expert_next_obs, mb_policy_next_obs))
+#                 mb_policy_action_log_probs = np.concatenate((mb_expert_action_log_probs_under_policy, mb_policy_action_log_probs))
+#                 global_session.run(
+#                     self.train_op,
+#                     feed_dict={self.obs: mb_obs, self.next_obs: mb_next_obs, self.policy_action_log_probs: mb_policy_action_log_probs, self.labels: labels}
+#                 )
+#
+#             loss = global_session.run(
+#                 self.loss,
+#                 feed_dict={self.obs: mb_obs, self.next_obs: mb_next_obs, self.policy_action_log_probs: mb_policy_action_log_probs, self.labels: labels}
+#             )
+#             print('discrim_loss:', loss)

envs/turtle_env.py
@@ -13,19 +13,20 @@ class TurtleEnv(gym.Env):
 
     def __init__(self):
         self.viewer = None
-        self.max_linear_vel = 10
+        self.max_linear_vel = 0.1
         self.max_angular_vel = 5
         self.dt = 0.05
+        self.target = np.random.rand(2)
 
-        self.height = 100.0
-        self.width = 100.0
+        self.height = 1
+        self.width = 1
         high = np.array([self.max_linear_vel, self.max_angular_vel])
 
         # Range of linear and angular velocities that the turtlebot accepts as input.
         self.action_space = spaces.Box(low=-high, high=high)
 
         # Range of coordinates and orientation angles which define the state space.
-        self.observation_space = spaces.Box(low=np.array([0, 0, 0]), high=np.array([self.width, self.height, 2*np.pi]))
+        self.observation_space = spaces.Box(low=np.array([0, 0, -np.pi, 0, 0, -np.pi]), high=np.array([self.width, self.height, np.pi, self.target[0], self.target[1], np.pi]))
 
         self.seed()
 
@@ -50,19 +51,36 @@ class TurtleEnv(gym.Env):
         new_x  = np.clip(x + dx, 0, self.width)
         new_y  = np.clip(y + dy, 0, self.height)
         new_th = th + dth
+        while new_th < -np.pi:
+            new_th += 2*np.pi
+        while new_th >= np.pi:
+            new_th -= 2*np.pi
         self.state = np.array([new_x, new_y, new_th])
 
-        costs = 0 # for now!
-        return self._get_obs(), -costs, False, {}
+        costs = np.linalg.norm(self.target - self.state[:2])
+        costs += self._get_angle()**2
+
+        done = (np.linalg.norm(self.target - self.state[:2]) < 0.01)
+        return self._get_obs(), -costs, done, {}
 
     def reset(self):
         # Start in the middle of the board with an angle of 0
-        self.state = np.array([self.width / 2, self.height / 2, 0])
+        # self.state = np.array([self.width / 2, self.height / 2, 0])
+        self.target = np.random.rand(2)
+        self.state = np.array([np.random.rand(), np.random.rand(), 2*np.pi*(np.random.rand()-0.5)])
         self.last_u = None
         return self._get_obs()
 
     def _get_obs(self):
-        return self.state
+        return np.concatenate((self.state, self.target, [self._get_angle()]))
+
+    def _get_angle(self):
+        angle = np.arctan2(self.target[1] - self.state[1], self.target[0] - self.state[0]) - self.state[2]
+        while angle < -np.pi:
+            angle += 2*np.pi
+        while angle >= np.pi:
+            angle -= 2*np.pi
+        return angle
 
     def render(self, mode='human'):
 
@@ -71,21 +89,29 @@ class TurtleEnv(gym.Env):
             self.viewer = rendering.Viewer(500,500)
             # self.viewer.set_bounds(-2.2,2.2,-2.2,2.2)
             self.viewer.set_bounds(0, self.width, 0, self.height)
-            turtle = rendering.make_circle(3)
+
+            turtle = rendering.make_circle(0.03)
             turtle.set_color(0, 0, 0)
             self.turtle_transform = rendering.Transform()
             turtle.add_attr(self.turtle_transform)
             self.viewer.add_geom(turtle)
 
+            target = rendering.make_circle(0.01)
+            target.set_color(0, 16, 0)
+            self.target_transform = rendering.Transform()
+            target.add_attr(self.target_transform)
+            self.viewer.add_geom(target)
+
         self.turtle_transform.set_translation(self.state[0], self.state[1])
+        self.target_transform.set_translation(self.target[0], self.target[1])
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
     def close(self):
         if self.viewer: self.viewer.close()
 
-
-env = TurtleEnv()
-env.reset()
-for _ in range(10000):
-    env.render()
-    env.step(env.action_space.sample())
+if __name__ == '__main__':
+    env = TurtleEnv()
+    env.reset()
+    for _ in range(100):
+        env.render()
+env.step((1, 10*env._get_angle()))

rewards.py
@@ -2,10 +2,10 @@ import numpy as np
 
 def make_irl_reward_fn(model, env_reward_weight=0, entropy_weight=0.1, discriminator_reward_weight=1):
     def irl_reward(obs, next_obs, actions, action_log_probs, env_rewards, values, entropies):
-        expert_probs = model.discriminator.expert_prob(obs, next_obs, action_log_probs, model.sess)
+        expert_log_probs = model.discriminator.expert_log_prob(obs, next_obs, action_log_probs, model.sess)
         reward = env_reward_weight*env_rewards \
             + entropy_weight*entropies \
-            + discriminator_reward_weight*(np.log(expert_probs+1e-8) - np.log(1-expert_probs+1e-8))
+            + discriminator_reward_weight*(expert_log_probs - np.log(1-np.exp(expert_log_probs)+1e-8))
         return reward
     return irl_reward
 