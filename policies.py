2e6fe5eec397da437e2a6e405600d88e9955b053
Ubuntu <ubuntu@ip-172-31-27-26.us-west-2.compute.internal>
2018-05-17 21:55:54 +0000

    model

--- policies.py
+++ policies.py
@@ -16,7 +16,7 @@ class GaussianMLPPolicy:
         var_network=False, # NN if true, else trainable params indep of obs
         out_activation=None,
         hidden_dims=[64, 64, 64],
-        hidden_activation=tf.nn.elu,
+        hidden_activation=tf.nn.relu,
         weight_init=tf.contrib.layers.xavier_initializer,
         bias_init=tf.zeros_initializer,
         optimizer=ClipPPO
